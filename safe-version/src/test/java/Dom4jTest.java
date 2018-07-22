795f132d20368481a21881552158d41e30f41714
Mário Areias <mario.s.s.areias@gmail.com>
2018-07-22 20:02:16 +1000

    Initial commit

---
+++ safe-version/src/test/java/Dom4jTest.java
@@ -0,0 +1,68 @@
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+import org.junit.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+
+public class Dom4jTest {
+
+    @Test
+    public void testXMLInjectionOnAttributeName() throws IOException {
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement("root");
+
+        String injectedTag = "<author name=\"hack\" location=\"World\">true</author>";
+
+        Element author = root.addElement("author")
+                .addAttribute("name=\"hack\" location=\"World\">true</author><author name", "James")
+                .addAttribute("location", "UK")
+                .addText("James Strachan");
+
+        Element author2 = root.addElement("author")
+                .addAttribute("name", "Bob")
+                .addAttribute("location", "US")
+                .addText("Bob McWhirter");
+
+        writeFile(document);
+
+        assertThat(document.asXML(), containsString(injectedTag));
+    }
+
+    private void writeFile(Document document) throws IOException {
+        try (FileWriter fileWriter = new FileWriter("output.xml")) {
+            XMLWriter writer = new XMLWriter(fileWriter, OutputFormat.createPrettyPrint());
+            writer.write( document );
+            writer.close();
+        }
+    }
+
+    @Test
+    public void testXMLInjectionOnTagName() throws IOException {
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement("root");
+
+        String injectedAttribute = "age=\"20\"";
+
+        Element author = root.addElement("author " + injectedAttribute)
+                .addAttribute("name", "James")
+                .addAttribute("location", "UK");
+
+        Element author2 = root.addElement("author")
+                .addAttribute("name", "Bob")
+                .addAttribute("location", "US")
+                .addText("Bob McWhirter");
+
+
+        writeFile(document);
+
+        assertThat(document.asXML(), containsString(injectedAttribute));
+    }
+}
